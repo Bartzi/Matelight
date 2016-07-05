21c333ad6e827875425da7635e6aa98ec480299a
Christian Bartz <christian.bartz@student.hpi.uni-potsdam.de>
2016-07-05 20:36:58 +0000

    first version of led controller

--- led_control.py
+++ led_control.py
@@ -11,8 +11,8 @@ class Crate(object):
         self.num_leds = height *  width
 
     def check_bounds(self, data):
-        assert data.shape[0] == self.height
-        assert data.shape[1] == self.width
+        assert data.shape[0] == self.width
+        assert data.shape[1] == self.height
 
     @property
     def crate_in_next_row(self):
@@ -41,7 +41,7 @@ class Crate(object):
 class BottomLeftCrate(Crate):
     @property
     def crate_in_next_row(self):
-        return 1, BottomRightCrate
+        return -1, BottomRightCrate
 
     @property
     def crate_in_next_column(self):
@@ -49,13 +49,13 @@ class BottomLeftCrate(Crate):
 
     def transform_pixels(self, data):
         self.check_bounds(data)
-        return self.reverse_every_odd_column(data])
+        return self.reverse_every_odd_column(data)
 
 
 class BottomRightCrate(Crate):
     @property
     def crate_in_next_row(self):
-        return 1, BottomLeftCrate
+        return -1, BottomLeftCrate
 
     @property
     def crate_in_next_column(self):
@@ -69,7 +69,7 @@ class BottomRightCrate(Crate):
 class TopLeftCrate(Crate):
     @property
     def crate_in_next_row(self):
-        return -1, TopRightCrate
+        return 1, TopRightCrate
 
     @property
     def crate_in_next_column(self):
@@ -83,7 +83,7 @@ class TopLeftCrate(Crate):
 class TopRightCrate(Crate):
     @property
     def crate_in_next_row(self):
-        return -1, TopLeftCrate
+        return 1, TopLeftCrate
 
     @property
     def crate_in_next_column(self):
@@ -93,29 +93,50 @@ class TopRightCrate(Crate):
         self.check_bounds(data)
         return self.reverse_every_even_column(data[::-1])
 
+NAME_TO_CRATE = {
+    "bottomleftcrate": BottomLeftCrate,
+    "bottomrightcrate": BottomRightCrate,
+    "topleftcrate": TopLeftCrate,
+    "toprightcrate": TopRightCrate,
+}
+
 
 class LEDController(object):
 
     def __init__(self, config_file, device_name="/dev/spidev0.0"):
-        self.config = configparser.ConfigParser()
+        self.config = configparser.ConfigParser(allow_no_value=True)
         self.config.read(config_file)
 
         self.crate_rows = int(self.config["Layout"]["crate_rows"])
         self.crate_columns = int(self.config["Layout"]["crate_columns"])
-        self.num_crates = crate_rows * crate_columns
-        self.crates = [((0,0), eval(self.config["Crates"][0]))]
-        for crate in self.config["Crates"][1:]:
-            crate_type = eval(crate)
+        self.num_crates = self.crate_rows * self.crate_columns
+
+        self.crate_width = int(self.config["Layout"]["crate_width"])
+        self.crate_height = int(self.config["Layout"]["crate_height"])
+        self.leds_per_crate = self.crate_width * self.crate_height
+
+        crates = list(self.config["Crates"].keys())
+        self.crates = [((0,0), NAME_TO_CRATE[crates[0]]())]
+        for crate in crates[1:]:
+            crate_type = NAME_TO_CRATE[crate]
             last_position, last_crate = self.crates[-1]
             row_displace, next_row_crate = last_crate.crate_in_next_row
             column_displace, next_column_crate = last_crate.crate_in_next_column
 
             if crate_type == next_row_crate:
-                self.crates.append(((last_position[0], last_position[1] + row_displace), crate_type()))
+                self.crates.append(((last_position[0], last_position[1] + row_displace), crate_type(width=self.crate_width, height=self.crate_height)))
             elif crate_type == next_column_crate:
-                self.crates.append(((last_position[0] + column_displace, last_position[1]), crate_type()))
+                self.crates.append(((last_position[0] + column_displace, last_position[1]), crate_type(width=self.crate_width, height=self.crate_height)))
             else:
-                raise ValueError("Your specification of crates seems not to be plausible! Please check that!")     
+                raise ValueError("Your specification of crates seems not to be plausible! Please check that!")
+
+        # adjust crate indices so that top left crate has index (0,0)
+        min_x = min(self.crates, key=lambda x: x[0][0])[0][0]
+        min_y = min(self.crates, key=lambda x: x[0][1])[0][1]
+        self.crates = [((x + abs(min_x), y), crate) for (x, y), crate in self.crates]
+        self.crates = [((x, y + abs(min_y)), crate) for (x, y), crate in self.crates]
+
+        self.crates.sort(key=lambda x: (x[0][1], x[0][0]))
 
         print(self.crates)
 
@@ -132,18 +153,23 @@ class LEDController(object):
         return real_data
 
     def display(self, data):
-        if len(data.shape) != 3 or data.shape[0] * data.shape[1] > self.num_leds:
+        if len(data.shape) != 3 or data.shape[0] * data.shape[1] != self.num_crates * self.leds_per_crate:
             print(data.shape)
             raise ValueError("Supplied data needs to have shape like (x, y, z), where z = [1,3] and x * y <= num_leds")
         # make data column major
-        data = np.transpose(data)
-
-        self.device.write(self.pad_unit_data(data).astype(np.uint8))
+        data = np.transpose(data, (1, 0, 2)).astype(np.uint8)
+        display_data = np.empty((0,), dtype=np.uint8)
+        for (x, y), crate in self.crates:
+            display_data = np.append(
+                display_data,
+                crate.transform_pixels(data[x * self.crate_width:(x + 1) * self.crate_width, y* self.crate_height:(y + 1) * self.crate_height]).flatten()
+            )
+        self.device.write(np.ascontiguousarray(data))
         self.device.flush()
         time.sleep(0.1)
 
     def turn_off_lights(self):
-        data = np.full((self.num_leds, 1, 3), 0, dtype=np.uint8)
+        data = np.full((self.num_crates * self.leds_per_crate, 1, 3), 0, dtype=np.uint8)
         self.display(data)
 
     def shutdown(self):
@@ -162,12 +188,12 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    data = np.full((args.num_leds, 1, 3), 0, dtype=np.int32)
+    data = np.full((8, 10, 3), 0, dtype=np.uint8)
     colors = np.vectorize(lambda x: random.randint(0, 255))
-    controller = LEDController(2, 2, BottomRightCrate())
-    # try:
-    #     while True:
-    #         controller.display(colors(data))
-    #         time.sleep(args.sleep_time / 1000)
-    # except KeyboardInterrupt:
-    #     controller.shutdown()
+    controller = LEDController(args.config)
+    try:
+        while True:
+            controller.display(colors(data))
+            time.sleep(args.sleep_time / 1000)
+    except KeyboardInterrupt:
+        controller.shutdown()
