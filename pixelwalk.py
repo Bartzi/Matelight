a7d109be10f50c4d4c60431e438f6679e63242e3
Hannes Rantzsch <hannes.rantzsch@student.hpi.de>
2016-11-26 22:54:17 +0100

    pixelserver and pixelengine

--- pixelwalk.py
+++ pixelwalk.py
@@ -3,14 +3,74 @@ import numpy as np
 from time import sleep
 import argparse
 
+import socketserver
+
 from cursesdisplay import CursesDisplay
 from led_control import LEDController
 
 
-if __name__ == '__main__':
-    WIDTH = 16
-    HEIGHT = 12
+class PixelEngine():
 
+    def __init__(self, args):
+        self.height = 12
+        self.width = 15
+        self.displays = []
+        if not args.emulate:
+            self.displays.append(LEDController(args.config))
+
+        self.displays.append(CursesDisplay.new(self.height, self.width+1))
+
+        self.position = (6, 8)
+
+    def handle(self, data):
+        y, x = self.position
+
+        k = chr(int(data))
+        if k == 'w':
+            y = max(0, y-1)
+        elif k == 's':
+            y = min(self.height-1, y+1)
+        elif k == 'a':
+            x = max(0, x-1)
+        elif k == 'd':
+            x = min(self.width-2, x+1)
+
+        self.position = (y, x)
+
+        output = np.zeros((self.height, self.width, 3), dtype=np.uint8)
+        output[y, x, 1] = 255
+
+        for d in self.displays:
+            d.display(output)
+
+
+class PixelServer(socketserver.UDPServer):
+
+    def __init__(self, *args, **kwargs):
+        self.controller = kwargs.pop('controller')
+        super(PixelServer, self).__init__(*args, **kwargs)
+
+    def finish_request(self, request, client_address):
+        self.RequestHandlerClass(
+            request,
+            client_address,
+            self,
+            controller=self.controller
+        )
+
+
+class PixelHandler(socketserver.DatagramRequestHandler):
+
+    def __init__(self, *args, **kwargs):
+        self.controller = kwargs.pop('controller')
+        super(PixelHandler, self).__init__(*args, **kwargs)
+
+    def handle(self):
+        data = self.rfile.read().decode("utf-8").strip()
+        self.controller.handle(data)
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('config', help="Path to config for matelight")
     parser.add_argument(
@@ -22,22 +82,10 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
-    if args.emulate:
-        display = CursesDisplay.new(HEIGHT, WIDTH)
-    else:
-        display = LEDController(args.config)
-
-    while True:
-        arr = np.random.randint(0, 255, size=(HEIGHT-1, WIDTH-1, 3))
-        display.display(arr)
-        sleep(0.5)
-
-#         k = chr(stdscr.getch())
-#         if k == 'w':
-#             y = max(0, y-1)
-#         elif k == 's':
-#             y = min(HEIGHT-1, y+1)
-#         elif k == 'a':
-#             x = max(0, x-1)
-#         elif k == 'd':
-#             x = min(WIDTH-2, x+1)
+    engine = PixelEngine(args)
+    server = PixelServer(
+        ("0.0.0.0", 9999),
+        PixelHandler,
+        controller=engine
+    )
+    server.serve_forever()
