779ac4857c5bfcb251d1f9da93c0498275d826e4
Johannes Jasper <mail@johannesjasper.de>
2016-11-26 23:51:02 +0100

    use different API to capture key down events

--- remote_client.py
+++ remote_client.py
@@ -1,28 +1,48 @@
 import configparser
-import curses
 import socket
+from time import sleep
 
 config = configparser.ConfigParser()
 config.read('config.cfg.default')
 
 
-class RemoteClient:
-    @classmethod
-    def new(cls):
-        return curses.wrapper(cls)
+def _find_getch():
+    try:
+        import termios
+    except ImportError:
+        # Non-POSIX. Return msvcrt's (Windows') getch.
+        import msvcrt
+        return msvcrt.getch
 
-    def __init__(self, screen):
-        self.screen = screen
+    # POSIX system. Create and return a getch that manipulates the tty.
+    import sys, tty
+    def _getch():
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(fd)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
 
-        while True:
-            k = self.screen.getch()
-            if chr(k) == 'q':
-                break
-
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.sendto(
-                str(k).encode(),
-                (config.get('Remote', 'host'), config.getint('Remote', 'port')))
+    return _getch
 
 
-RemoteClient.new()
+getch = _find_getch()
+
+if __name__ == '__main__':
+    while True:
+        # TODO remove
+        # replace this with some queue that limits the number
+        # of events to be sent over the wire
+        sleep(.1)
+
+        k = getch()
+        if k == 'q':
+            break
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(
+            str(ord(k)).encode(),
+            (config.get('Remote', 'host'), config.getint('Remote', 'port')))
